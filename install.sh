deeffdec6a2b40391d293e2661f7f6474f72d4bc
Phanatos- <pbonsembiante@gmail.com>
2018-04-04 21:38:18 -0300

    Added bash install script

---
+++ install.sh
@@ -0,0 +1,20 @@
+echo "Getting exercises directory..."
+
+dir=$(exercism debug 2>/dev/null | grep "Exercises Directory" 2>/dev/null)
+dir=$(awk '{print $3}' <<< $dir)
+
+git_dir=$(echo $dir/.git/)
+
+if [ -d $git_dir ]
+then
+    echo "Directory found at:" $dir
+
+    hookfile=$(echo $git_dir/hooks/pre-commit)
+    if [ -n $hookfile ]
+    then
+        cp -f pre-commit.py $hookfile
+    fi
+
+else
+    echo "A git repository must exist in " $dir
+fi
